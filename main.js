26e7eec614f02f6af13f0703e2fa2dc22b3a3d7c
Yaya Baldé <yaya.baldinho9@gmail.com>
2023-11-21 18:13:46 +0100

    update

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ links.forEach((link) => {
     nav.classList.remove("active")
     })
 })
+
+// AJOUT DE L'EFFET ECRITURE EN PAGE D'ACCUEIL
+const ecriture = document.querySelector("#ecriture")
+textContent = "Je suis Yaya BALDE EN FORMATION DE DEVELOPEMENT WEB"
+let index = 0
+function typeText() {
+  ecriture.textContent = textContent.slice(0, index);
+  index++;
+
+  if (index <= textContent.length) {
+      setTimeout(typeText, 100); // ajustez la vitesse de frappe en millisecondes
+  }
+}
+typeText()
